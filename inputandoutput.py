cb80cfa2477295d8d4635ca782c1c666084afe8e
Mathan Kumar <mathankumar01234mmk@gmail.com>
2026-03-31 21:28:51 +0530

    input and output in python

---
+++ inputandoutput.py
@@ -0,0 +1,21 @@
+#input concept: it pause the program and get the input from the user in the console and returned the value as string
+# if we need the input as float or int ,we must convert it manually using type casting
+#name=input("Enter Your Name:")
+#print("Your name is",name)
+
+#firstname,lastname=input("Enter Your Name:").split()
+#print('Your First Name:',firstname," and Your Last Name:",lastname)
+
+#var1=int(input("Enter Your Age:"))
+#print(type(var1))
+
+#var2=float(input("Enter Your cpga:"))
+#print(type(var2))
+
+#output concept--print() it will display the output in the console
+#name='mathan'
+#age=24
+#city='alangulam'
+#print(name,age,city)
+
+
