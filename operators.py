101843c3aeaba45610badbc387e14d836f208b42
Mathan Kumar <mathankumar01234mmk@gmail.com>
2026-04-08 22:23:43 +0530

    learning python

---
+++ operators.py
@@ -0,0 +1,42 @@
+a=10
+b=11
+#Arithmetic Operator
+#addition
+print("Addition of a+b=",a+b)
+
+#subraction
+print("Subraction of a-b=",a-b)
+
+#divion(floating number)
+print("Division of a/b=",a/b)
+
+#division(floor number)
+print("Floor Division of a//b=",a//b)
+
+#Multiplication
+print("Multiplication of a x b=",a*b)
+
+#exponentiation
+#now new object create for 2 and 10 ,then x and y reference the object
+a=2
+b=10
+print("Exponentiation of a ** b=",a**b)
+
+#modulus
+print("Modulus of a % b=",a%b)
+
+#Relational OR Comparison Operator
+
+# == and is both used for comparison
+#== (value comparison of two objects)
+print(a==b)
+
+#is (memory location comparison of two objects)
+print(a is b)
+
+a=10
+b=a
+print(a is b)
+
+
+
